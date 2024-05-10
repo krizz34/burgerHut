a0bf0ee6cd8353cedfa7c5420da44c9ebe27cd8f
Ananda Krishna P <125692378+krizz34@users.noreply.github.com>
2024-05-10 12:58:45 +0530

    Update OrderRepository.java on ospyn training

--- burgerHut/burgerHut/src/main/java/com/krizz34/burgerHut/repository/OrderRepository.java
+++ burgerHut/burgerHut/src/main/java/com/krizz34/burgerHut/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<orders,Long> {
     List<orders> findByStatus(String status);
 }
